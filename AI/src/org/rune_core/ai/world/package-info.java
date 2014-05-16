adf1045a5ebc73db9171d2e0a870acdc42e639a1
Dillusion <grabnoc@gmail.com>
2014-05-15 20:22:03 -0400

    Removal

--- AI/src/org/rune_core/ai/world/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * The <b>world</b> package contains classes directly involved with maintaining
- * as well is creating the game world environment, including the threads needed
- * for the game world to run properly.
- * 
- * @author Grabn_000
- * @version 0.1
- */
-package org.rune_core.ai.world;
--- AI/src/org/rune_core/ai/world/util/pulse/PulseState.java
+++
@@ -1,9 +0,0 @@
-package org.rune_core.ai.world.util.pulse;
-
-public enum PulseState {
-
-	ALIVE,
-	
-	DEAD;
-	
-}
